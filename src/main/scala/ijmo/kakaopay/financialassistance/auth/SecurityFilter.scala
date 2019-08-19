478e1dc759c129d55a773000a8fdef7dba87f570
ijmo <jinmo.im@gmail.com>
2019-08-19 14:45:05 +0900

    Add exclusion for actuator

--- src/main/scala/ijmo/kakaopay/financialassistance/auth/SecurityFilter.scala
+++ src/main/scala/ijmo/kakaopay/financialassistance/auth/SecurityFilter.scala
@@ -4,9 +4,11 @@ import ijmo.kakaopay.financialassistance.system.SecurityConfig
 import io.jsonwebtoken.Jwts
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 import javax.servlet.{Filter, FilterChain, ServletRequest, ServletResponse}
+import org.slf4j.{Logger, LoggerFactory}
 
 object SecurityFilter {
   val excludedUris = "/api/signup,/api/signin"
+  val logger: Logger = LoggerFactory.getLogger(SecurityFilter.getClass)
 }
 
 class SecurityFilter extends Filter {
@@ -16,18 +18,18 @@ class SecurityFilter extends Filter {
     val httpServletResponse: HttpServletResponse = response.asInstanceOf[HttpServletResponse]
 
     val uri = httpServletRequest.getServletPath
-    if (SecurityFilter.excludedUris.contains(uri)) {
+    if (SecurityFilter.excludedUris.contains(uri) || uri.toLowerCase.startsWith("/actuator")) {
       chain.doFilter(request, response)
       return
     }
 
     try {
       val token: String = httpServletRequest.getHeader("Authorization")
-      val jwt = if (token != null && token.toLowerCase.startsWith("bearer ")) token.drop(7) else throw new Exception("Unauthorized")
+      val jwt = if (token != null && token.startsWith("Bearer ")) token.drop(7) else throw new Exception("Unauthorized: Failed to get bearer token")
       val jws = Jwts.parser().setSigningKey(SecurityConfig.MY_SECRET_KEY).parseClaimsJws(jwt)
     } catch {
       case e: Throwable =>
-        println(e.getMessage)
+        SecurityFilter.logger.warn(e.getMessage)
         httpServletResponse.reset()
         httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED)
         return
