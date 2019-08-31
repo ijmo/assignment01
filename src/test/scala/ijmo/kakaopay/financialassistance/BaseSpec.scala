6bd5f24f64421cc2a4486e6899df565b52a22f5a
ijmo <jinmo.im@gmail.com>
2019-08-31 18:18:52 +0900

    Change BeforeAndAfter to BeforeAndAfterEach

--- src/test/scala/ijmo/kakaopay/financialassistance/BaseSpec.scala
+++ src/test/scala/ijmo/kakaopay/financialassistance/BaseSpec.scala
@@ -1,8 +1,8 @@
 package ijmo.kakaopay.financialassistance
 
-import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, FeatureSpec, GivenWhenThen, Matchers}
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FeatureSpec, GivenWhenThen, Matchers}
 import org.springframework.test.context.TestContextManager
 
-trait BaseSpec extends FeatureSpec with GivenWhenThen with Matchers with BeforeAndAfter with BeforeAndAfterAll {
+trait BaseSpec extends FeatureSpec with GivenWhenThen with Matchers with BeforeAndAfterEach with BeforeAndAfterAll {
   new TestContextManager(this.getClass).prepareTestInstance(this)
 }
