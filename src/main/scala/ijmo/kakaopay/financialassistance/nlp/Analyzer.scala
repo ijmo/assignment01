2f79e7da46fe6bab6b49a7f6e593ce829a1e8d53
ijmo <jinmo.im@gmail.com>
2019-08-31 14:02:24 +0900

    Add addUserDictionary function

--- src/main/scala/ijmo/kakaopay/financialassistance/nlp/Analyzer.scala
+++ src/main/scala/ijmo/kakaopay/financialassistance/nlp/Analyzer.scala
@@ -2,10 +2,17 @@ package ijmo.kakaopay.financialassistance.nlp
 
 import org.bitbucket.eunjeon.seunjeon
 
-import scala.collection.Iterable
+import scala.collection.{Iterable, mutable}
 
 object Analyzer {
-  def parse(s: String): Iterable[Morpheme] = seunjeon.Analyzer.parse(s).map(_.morpheme).map(m => Morpheme(m.getSurface, m.getFeatureHead))
-  def setUserDictionary(s: Iterator[String]): Unit = seunjeon.Analyzer.setUserDict(s)
-  def analyzer = seunjeon.Analyzer
+  private val analyzer = seunjeon.Analyzer
+  private val userDictionary: mutable.HashSet[String] = mutable.HashSet()
+
+  def parse(s: String): Iterable[Morpheme] = analyzer.parse(s).map(_.morpheme).map(m => Morpheme(m.getSurface, m.getFeatureHead))
+
+  def addUserDictionary(s: String): Unit = {
+    if (userDictionary.contains(s)) return
+    userDictionary += s
+    analyzer.setUserDict(userDictionary.toIterator)
+  }
 }
