e5e2e05a1e107084f17289ad9d522ce735b0f256
ijmo <jinmo.im@gmail.com>
2019-08-31 13:58:25 +0900

    Fix Id Generator to use sequence table

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
-INSERT INTO user (id, username, password, authorities, created_on) values (1, 'admin', '{noop}admin', 'ROLE_USER,ROLE_ADMIN', CURRENT_TIMESTAMP());
-INSERT INTO user (id, username, password, authorities, created_on) values (2, 'user', '{noop}1234', 'ROLE_USER', CURRENT_TIMESTAMP());
+CREATE TABLE organization_sequence (next_val BIGINT(19));
+INSERT INTO organization_sequence (next_val) VALUES (1);
+
+INSERT INTO user (id, username, password, authorities, created_on) VALUES (1, 'admin', '{noop}admin', 'ROLE_USER,ROLE_ADMIN', CURRENT_TIMESTAMP());
+INSERT INTO user (id, username, password, authorities, created_on) VALUES (2, 'user', '{noop}1234', 'ROLE_USER', CURRENT_TIMESTAMP());
